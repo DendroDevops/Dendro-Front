9dffa7e946b4ff85f03f38eea819032a9ce50b45
cain1919 <kabil.benhamda@esprit.tn>
2023-02-03 20:29:41 +0100

    feat:mod Dockerfile-dev for dev + update environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  baseUrl: 'https://test-dendromap.com/api/',
+  baseUrl: 'https://dendromap.fr/api/',
   headers :new HttpHeaders({
  
     'Content-Type':'undefined',
